c504d4011df5d861f15a28c1c03c4f3b7be792f8
BAHROMTJ <sarifzodabahromofficall@gmail.com>
2023-05-12 09:28:26 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,139 @@
+// function yearsInOneHouse(age, moves) {
+// 	if(moves===0)
+// 	{return age } else return Math.round(age/(moves+1))
+
+// }
+
+
+
+
+// function newWord(str) {
+// 	return str.slice(1)
+// }
+
+
+// function flipBool(b) {
+// 	return Number(b!=true)
+// }
+
+
+// const helloName = name =>"Hello "+name+'!'
+
+
+
+
+// function isEvenOrOdd(num) {
+// 	return num%2==0?"even":"odd"
+// }
+
+
+// function isOdd(num) {
+// 	return num % 2 !==0;
+// }
+
+
+// function stackBoxes(n) {
+// 	return n*n
+// }
+
+
+// function areaShape(base, height, shape) {
+// 	return shape=="triangle"?0.5 * base * height:base * height
+// }
+
+
+// function arrayToString(arr) {
+// 	return arr.join("")
+// }
+
+
+// function concat(arr1, arr2) {
+// 	return arr1.concat(arr2)
+// }
+
+
+// function findIndex(arr, str) {
+// 	return arr.indexOf(str)
+// }
+
+
+
+
+// function valueAt(arr, i) {
+// 	let b=Math.floor(i)
+// 	return arr[b]
+// }
+
+
+
+// function search(arr, item) {
+// 	return arr.indexOf(item)
+// }
+
+
+
+// function sumArray(arr) {
+// 	let sum=0
+//   for( let i=0;i<arr.length;i++) {
+// 	  sum+=arr[i]
+// 	}
+//   return sum
+// }
+
+
+
+// function search(arr, item) {
+//     return arr.indexOf(item)
+//    }
+
+
+
+
+// function check(arr, el) {
+// 	return arr.includes(el)
+// }
+
+
+
+// function parity(n) {
+//     let remander=n%2!=0
+//         if(remander === false) {
+//             return "even"
+//         }
+    
+//             return "odd"
+//     }
+
+
+
+
+// function parseArray(arr) {
+// 	return arr.map((num) => num.toString());
+// }
+
+
+
+
+
+// function howManyStickers(n) {
+// 	return n*6*n
+// }
+
+
+
+// function preventChanges(obj) {
+//     // write your code here
+//       // don't use a return statement
+      
+      
+//     // DON'T CHANGE OR REMOVE THE LINES BELOW
+//     obj.noChanges = true;
+//     obj.noChanges = true
+//     return obj;
+//   }
+
+
+
+// function carsNeeded(n) {
+// 	return Math.ceil(n/5)
+// }
